fe9c56ae9b20de860781d9c0450526e910b4a786
Muhammet Mesut Koç <mestkoc34@hotmail.com>
2023-02-28 21:53:09 +0300

    first

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 const App = ({ page }) => {
     return (
         <div className="app">
-            <h1>Hello world! I am using React</h1>
+            <h1>Hello world! I am using React!</h1>
             <h6>{page}</h6>
         </div>
     )
